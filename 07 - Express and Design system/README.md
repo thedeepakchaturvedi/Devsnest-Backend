7f6bbdd3dc503a68e80c67433b5b32397645fb65
deepakchaturvedifzd <deepakchaturvedifzd@gmail.com>
2021-09-09 13:58:45 +0530

    updated day 7

---
+++ 07 - Express and Design system/README.md
@@ -0,0 +1,23 @@
+> to create package.json
+
+```
+npm init
+```
+
+> installing express
+
+```
+npm install --save express
+```
+
+> installing nodemon globally
+
+```
+npm install -g nodemon
+```
+
+> to make nodemon listen index.js
+
+```
+nodemon index.js
+```
--- 07 - Express and Design system/index.js
+++ 07 - Express and Design system/index.js
@@ -4,7 +4,12 @@ const app = express();
 // CRUD -> POST, GET, PUT, DELETE
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.status(200).send("Hello");
+});
+
+app.get("/home", (req, res) => {
+  res.send(req.query); // returns all the params provided in the url
+  // url?a=50&b=xyz
 });
 
 app.post("/", (req, res) => {
@@ -13,6 +18,32 @@ app.post("/", (req, res) => {
 
 app.put("/", (req, res) => {
   res.send("Hello PUT");
+});
+
+////////////////////// conditions /////////////////////////
+
+app.get("/a(b)?cd", (req, res) => {
+  res.send("a(b is optional)cd");
+});
+
+app.get("/ab(c)+de", (req, res) => {
+  res.send("ab(no limit on c)de");
+});
+
+app.get("/xy*ab", (req, res) => {
+  res.send("xy(anything in between)ab");
+});
+
+app.get("/xy*ab", (req, res) => {
+  res.send("xy(anything in between)ab");
+});
+
+app.get(/k/, (req, res) => {
+  res.send("use of regex starting with k");
+});
+
+app.get("/user/:userid/student/:studentid", (req, res) => {
+  res.send(req.params); // http://localhost:5000/user/6/student/1
 });
 
 app.listen(5000);
